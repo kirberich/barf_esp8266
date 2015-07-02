77e1b12e79baf22c6e345d0140aaf5a3b4362f85
Robert Kirberich <robert@kirberich.de>
2015-07-02 23:38:27 +0100

    Go back to #define-ing constants

--- barf_constants.h
+++ barf_constants.h
@@ -1,39 +1,39 @@
 // Constants used both by the firmware and the barf library
 #pragma once
 
-const bool METHOD_GET = 0;
-const bool METHOD_POST = 1;
+#define METHOD_GET  0
+#define METHOD_POST  1
 
-const int LED_ACTIVITY = 0;
-const int LED_CONNECTION = 1;
-const int LED_OFF = 2;
-const int LED_ON = 3;
-const int LED_GPIO = 4;
+#define LED_ACTIVITY  0
+#define LED_CONNECTION  1
+#define LED_OFF 2
+#define LED_ON 3
+#define LED_GPIO 4
 
-const String ERROR = "__err__";
-const String TIMEOUT = "__timeout__";
-const String UNEXPECTED_COMMAND = "__unexpected_command__";
+#define ERROR "__err__"
+#define TIMEOUT "__timeout__"
+#define UNEXPECTED_COMMAND "__unexpected_command__"
 
-const String COMMAND_DEBUG = "debug";
-const String COMMAND_METHOD = "method";
-const String COMMAND_NUM_FRAMENTS = "num_fragments";
-const String COMMAND_PATH_FRAGMENT = "path_frament";
-const String COMMAND_GET_VAR = "get_var";
-const String COMMAND_GET_VALUE = "get_value";
-const String COMMAND_REQUEST_RESPONSE = "respond";
-const String COMMAND_RESPONSE_START = "response_start";
-const String COMMAND_RESPONSE_END = "response_end";
+#define COMMAND_DEBUG "debug"
+#define COMMAND_METHOD "method"
+#define COMMAND_NUM_FRAMENTS "num_fragments"
+#define COMMAND_PATH_FRAGMENT "path_frament"
+#define COMMAND_GET_VAR "get_var"
+#define COMMAND_GET_VALUE "get_value"
+#define COMMAND_REQUEST_RESPONSE "respond"
+#define COMMAND_RESPONSE_START "response_start"
+#define COMMAND_RESPONSE_END "response_end"
 
-const String COMMAND_SSID = "ssid";
-const String COMMAND_PASSWORD = "password";
-const String COMMAND_CONNECT = "connect";
-const String COMMAND_DISCONNECT = "disconnect";
-const String COMMAND_TIMEOUT = "timeout";
-const String COMMAND_LED_MODE = "led_mode";
-const String COMMAND_GET = "get";
-const String COMMAND_POST = "post";
-const String COMMAND_ALLOW_GPIO = "allow_gpio";
-const String COMMAND_DISALLOW_GPIO = "disallow_gpio";
-const String COMMAND_BAUD_RATE = "baud_rate";
-const String COMMAND_IS_CONNECTED = "is_connected";
-const String COMMAND_GET_IP = "get_ip";
+#define COMMAND_SSID "ssid"
+#define COMMAND_PASSWORD "password"
+#define COMMAND_CONNECT "connect"
+#define COMMAND_DISCONNECT "disconnect"
+#define COMMAND_TIMEOUT "timeout"
+#define COMMAND_LED_MODE "led_mode"
+#define COMMAND_GET "get"
+#define COMMAND_POST "post"
+#define COMMAND_ALLOW_GPIO "allow_gpio"
+#define COMMAND_DISALLOW_GPIO "disallow_gpio"
+#define COMMAND_BAUD_RATE "baud_rate"
+#define COMMAND_IS_CONNECTED "is_connected"
+#define COMMAND_GET_IP "get_ip"
